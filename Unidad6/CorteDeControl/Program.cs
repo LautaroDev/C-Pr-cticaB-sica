800255567fea70193914b2645769c95bf5955666
Lautaro <lautaroencalado@gmail.com>
2025-07-30 02:26:23 -0300

    Test

---
+++ Unidad6/CorteDeControl/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace CorteDeControl;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // CORTE DE CONTROL
+        while (true)
+        {
+            while (true)
+            {
+                
+            }
+        }
+    }
+}
---
+++ Unidad6/Ejemplo/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Ejemplo;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Ciclos combinados:
+        // For con While:
+        int n, prom;
+
+        Console.WriteLine("Ingrese 1 para comenzar a procesar o 0 para terminar: ");
+        n = int.Parse(Console.ReadLine());
+        while (n != 0)
+        {
+            int acum = 0;
+            for (int i = 0; i < 20; i++)
+            {
+                Console.WriteLine("Ingrese la edad : ");
+                n = int.Parse(Console.ReadLine());
+                acum += n;
+            }
+            prom = acum / 20;
+            Console.WriteLine("El promedio es: " + prom);
+            
+        Console.WriteLine("Ingrese 1 para comenzar a procesar o 0 para terminar: ");
+        n = int.Parse(Console.ReadLine());
+        }
+        
+    }
+}
