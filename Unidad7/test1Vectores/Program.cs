fe4451aab28221fcf2eed9dc3b8aa0970322b639
Lautaro <lautaroencalado@gmail.com>
2025-08-10 03:26:13 -0300

    Add initial project files and configurations for test1Vectores and test2Vectores
    - Created test1Vectores and test2Vectores projects targeting .NET 9.0.
    - Added necessary assembly attributes and configuration files.
    - Included generated files for build outputs, dependencies, and project settings.
    - Established project structure with appropriate .csproj files and settings.cls

---
+++ Unidad7/test1Vectores/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace test1Vectores;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n,acu=0;
+        //vectores (array o arreglo) 
+        int[] numeros = new int[10];
+
+        //cargar 10 numeros
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine("Ingresar un numero:");
+            numeros[i] = int.Parse(Console.ReadLine());
+        }
+
+        // mostrar los valores que agregamos 
+        for (int i = 0; i < 10; i++) //como el ciclo for anterior ya termino, puedo volver a usar la variable i
+        {
+            Console.WriteLine("Los numeros que tiene el vector son: " + numeros[i]);
+        }
+        //sumar todos los valores que ingresamo
+        for (int i = 0; i < 10; i++)
+        {
+            acu += numeros[i];
+        }
+            Console.WriteLine("La suma de todos los numeros es : " + acu);
+        
+
+        
+    }
+}
